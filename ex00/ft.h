35375f4902e4f5013b8ef73242e835386ac52617
Yann Dumaine <yannlescoul79f>
2022-02-01 17:03:12 +0100

    deboguage ex05 et normi other ex + suppr parasite files

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,15 +6,15 @@
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 12:04:07 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/01/31 12:12:08 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/02/01 17:02:39 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
-#define FT_H
+# define FT_H
 
 void	ft_putchar(char c);
-void 	ft_swap(int	*a,int	*b);
+void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s12, char *s2);
