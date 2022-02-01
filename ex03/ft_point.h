ae58c737341bc65bc0365b4e05d21eb502c84327
Yann Dumaine <yannlescoul79f>
2022-02-01 20:22:27 +0100

    ex04 probleme de retour de poiinteur et probleme dadressage

--- ex03/ft_point.h
+++ ex03/ft_point.h
@@ -6,18 +6,18 @@
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 13:10:50 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/02/01 16:52:50 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/02/01 19:31:44 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-typedef struct point	t_point;
 struct s_point
 {
 	int	x;
 	int	y;
 };
 
+typedef struct s_point	t_point;
 #endif 
--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 14:49:23 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/02/01 16:46:24 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/02/01 20:19:33 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	*ft_strcpy(char *src)
 	while (src[i] != '\0')
 		i++;
 	i++;
-	dest = malloc(i);
+	dest = malloc(sizeof(char) * i);
 	if (dest == NULL)
 		return (dest);
 	i = 0;
@@ -55,16 +55,16 @@ struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	i = 0;
 	tab = malloc(sizeof(t_stock_str) * (ac + 1));
 	if (tab == NULL)
-		return (tab);
+		return (NULL);
 	while (i < ac)
 	{
 		tab[i].size = ft_strlen(av[i]);
-		tab[i].str = ft_strcpy(av[i]);
+		tab[i].str = av[i];
 		tab[i].copy = ft_strcpy(av[i]);
+		if (tab[i].copy == NULL)
+			return (NULL);
 		i++;
 	}
 	tab[i].str = 0;
-	tab[i].copy = 0;
-	tab[i].size = 0;
 	return (tab);
 }
